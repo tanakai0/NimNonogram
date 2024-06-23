65b61327cd23bba3b929f20808147d5b2a4d0b6a
tanakai0 <tanakatanakakai@gmail.com>
2024-06-23 14:53:23 +0900

    update

--- src/NimNonogram.nim
+++ src/NimNonogram.nim
@@ -1,7 +1,54 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
+import std/parseopt
+import NimNonogrampkg/[solver, puzzleMaker, help]
 
-import NimNonogrampkg/submodule
+proc cli() =
+  var
+    filePath: string = ""
+    imagePath: string = ""
+    helpFlag: bool = false
+    solveFlag: bool = false
+    makePuzzleFlag: bool = false
+    argCtr: int = 0
+
+  for kind, key, value in getOpt():
+    case kind
+    of cmdArgument:
+      echo "# Positional argument ", argCtr, ": \"", key, "\""
+      argCtr.inc
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "solve":
+        solveFlag = true
+        filePath = value
+      of "make_puzzle":
+        makePuzzleFlag = true
+        imagePath = value
+      of "help", "h":
+        helpFlag = true
+      else:
+        echo "Unknown option: ", key
+        showHelp()
+        quit(1)
+    of cmdEnd:
+      discard
+
+  if helpFlag or (not solveFlag and not makePuzzleFlag):
+    showHelp()
+  elif solveFlag:
+    if filePath == "":
+      echo "Error: --solve requires a file path."
+      showHelp()
+      quit(1)
+    solve(filePath)
+  elif makePuzzleFlag:
+    if imagePath == "":
+      echo "Error: --make_puzzle requires an image path."
+      showHelp()
+      quit(1)
+    makePuzzle(imagePath)
 
 when isMainModule:
-  echo(getWelcomeMessage())
+  # Example to use
+  # .\src\NimNonogram.exe --solve: puzzlePath
+  # .\src\NimNonogram.exe --make_puzzle: puzzlePath
+  cli()
--- tests/test1.nim
+++ tests/test1.nim
@@ -5,8 +5,57 @@
 #
 # To run these tests, simply execute `nimble test`.
 
-import unittest
+import std/[unittest, strutils]
+import NimNonogrampkg/types
 
-import NimNonogrampkg/submodule
-test "correct welcome":
-  check getWelcomeMessage() == "Hello, World!"
+suite "Nonogram Tests":
+
+  test "correct countStateInRow":
+    var n = newNonogram(5, 5)
+    n.grid[0][0] = CellState.black
+    n.grid[0][1] = CellState.black
+    n.grid[0][2] = CellState.white
+    n.grid[0][3] = CellState.unknown
+    n.grid[0][4] = CellState.black
+    
+    check(countStateInRow(n, CellState.black, 0) == 3)
+    check(countStateInRow(n, CellState.white, 0) == 1)
+    check(countStateInRow(n, CellState.unknown, 0) == 1)
+
+  test "correct countStateInColumn":
+    var n = newNonogram(5, 5)
+    n.grid[0][0] = CellState.black
+    n.grid[1][0] = CellState.black
+    n.grid[2][0] = CellState.white
+    n.grid[3][0] = CellState.unknown
+    n.grid[4][0] = CellState.black
+    
+    check(countStateInColumn(n, CellState.black, 0) == 3)
+    check(countStateInColumn(n, CellState.white, 0) == 1)
+    check(countStateInColumn(n, CellState.unknown, 0) == 1)
+
+  test "correct countStateInGrid":
+    var n = newNonogram(5, 5)
+    n.grid[0][0] = CellState.black
+    n.grid[1][1] = CellState.black
+    n.grid[2][2] = CellState.white
+    n.grid[3][3] = CellState.unknown
+    n.grid[4][4] = CellState.black
+    
+    check(countStateInGrid(n, CellState.black) == 3)
+    check(countStateInGrid(n, CellState.white) == 1)
+    check(countStateInGrid(n, CellState.unknown) == 25 - 4)
+
+  test "toString produces correct output":
+    var n = newNonogram(2, 2)
+    n.grid[0][0] = CellState.black
+    n.grid[0][1] = CellState.white
+    n.grid[1][0] = CellState.unknown
+    n.grid[1][1] = CellState.black
+    
+    let output: string = n.toString()
+    check(output.contains("Nonogram(rows: 2, columns: 2)\n"))
+    check(output.contains("1 0 \n"))
+    check(output.contains("2 1 \n"))
+
+# To run these tests, simply execute `nimble test`.
