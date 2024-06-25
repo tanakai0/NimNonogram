cecf441453703ad30d094fdc848f40f5f38210be
tanakai0 <tanakatanakakai@gmail.com>
2024-06-25 23:50:39 +0900

    add proc stopTimer

--- src/NimNonogrampkg/workTable.nim
+++ src/NimNonogrampkg/workTable.nim
@@ -7,9 +7,9 @@ type
   WorkTable* = object
     nonogram*: Nonogram
     startTime: float  # Start time for time measurement
+    endTime: float  # End time for time measurement
     coloringOrder*: ColoringOrder
     solver*: NonogramSolver
-
 
 
 # Function to create a new WorkTable
@@ -18,3 +18,8 @@ proc newWorkTable*(nonogram: Nonogram, solver: NonogramSolver): WorkTable =
   result.startTime = cpuTime()
   result.coloringOrder = newColoringOrder(nonogram)
   result.solver = solver
+  return result
+
+# stopTime measures the elapsed time
+proc stopTimer*(workTable: var WorkTable) =
+  workTable.endTime = cpuTime() - workTable.startTime
