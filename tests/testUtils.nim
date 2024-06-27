6150197cd97d4b515246d08df8529de78d988a09
tanakai0 <tanakatanakakai@gmail.com>
2024-06-27 19:10:28 +0900

    init utils test

---
+++ tests/testUtils.nim
@@ -0,0 +1,36 @@
+import std/unittest
+import NimNonogrampkg/[nonogram, constants, utils]
+
+suite "Utils Tests":
+
+  test "correct row2hint":
+    let
+      row1: seq[CellState] = @[white, white, black, black, black, black, white, white]
+      row2: seq[CellState] = @[white, black, black, white, white, black, black, black]
+      row3: seq[CellState] = @[black, black, black, black, black, black, black, black]
+      row4: seq[CellState] = @[white, white, white, white, white, white, white, white]
+    
+    check(row2hint(row1) == @[4])
+    check(row2hint(row2) == @[2, 3])
+    check(row2hint(row3) == @[8])
+    check(row2hint(row4) == newSeq[int]())
+
+  test "correct isSolved":
+    var
+      nono: Nonogram = loadPuzzle(constants.ExamplePuzzlePath)
+      answer: seq[CellState] = @[
+        white, white, black, black, black, black, white, white,
+        white, black, black, white, white, black, black, white,
+        black, black, white, white, white, white, black, black,
+        black, black, black, black, black, black, black, black,
+        black, black, white, white, white, white, white, white,
+        black, black, white, white, white, white, black, black,
+        white, black, black, white, white, black, black, white,
+        white, white, black, black, black, black, white, white
+      ]
+    for i in 0..<nono.numRows:
+      for j in 0..<nono.numCols:
+        nono.grid[i][j] = answer[i * nono.numCols + j]
+    check(isSolved(nono) == true)
+    nono.grid[0][0] = black
+    check(isSolved(nono) == false)
