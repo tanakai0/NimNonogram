eaadfd47a21164ef50e24c329577fc1090158da6
tanakai0 <tanakatanakakai@gmail.com>
2024-06-30 11:28:59 +0900

    Change data structure
    Nonogram & coloringOrder <(used in) WorkTable <(used in) NonogramSolver

--- src/NimNonogrampkg/solvers/solvers.nim
+++ src/NimNonogrampkg/solvers/solvers.nim
@@ -1,17 +1,19 @@
-import std/deques
-import ../[nonogram, coloringLog]
+import ../workTable
 
 type
   NonogramSolver* = ref object of RootObj
+    workTable*: WorkTable
 
 type
   TestNonogramSolver* = ref object of NonogramSolver
     param1*: string
 
+proc newNonogramSolver*(workTable: WorkTable): NonogramSolver = 
+  result = NonogramSolver(workTable: workTable)
+  return result 
 
-proc newTestNonogramSolver*(param1: string): TestNonogramSolver = 
-  result = TestNonogramSolver(param1: param1)
-  result.param1 = param1
+proc newTestNonogramSolver*(workTable: WorkTable, param1: string): TestNonogramSolver = 
+  result = TestNonogramSolver(workTable: workTable, param1: param1)
   return result
 
 ## solve proc. starts to solve puzzle
@@ -23,9 +25,7 @@ method solve*(solver: TestNonogramSolver): bool =
   echo "solve in TestNonogramSolver"
   return true
 
-proc setAndLogCellState*(nonogram: var Nonogram, coloringLog: var ColoringLog, row: int, col: int, state: CellState): bool = 
-  if nonogram.setCellState(row, col, state):
-    coloringLog.addLast((rowInd: row, colInd: col, color: state))
-    return true
-  else:
-    return false
+when isMainModule:
+  let wt = WorkTable()
+  let solver = newTestNonogramSolver(wt, "test")
+  discard solver.solve()
--- src/NimNonogrampkg/workTable.nim
+++ src/NimNonogrampkg/workTable.nim
@@ -1,6 +1,6 @@
 import times
-import nonogram, coloringLog, solvers/solvers
-
+import std/deques
+import nonogram, coloringLog
 
 ## WorkTable collects information to solve a puzzle.
 type
@@ -9,16 +9,45 @@ type
     startTime: float  # Start time for time measurement
     endTime: float  # End time for time measurement
     coloringLog*: ColoringLog
-    solver*: NonogramSolver
+    totalUnknown*: int  # The number of unkown cells in the grid
+    rowUnknown*: seq[int]  # The number of unkown cells in each row
+    colUnknown*: seq[int]  # The number of unkown cells in each column
 
-# Function to create a new WorkTable
-proc newWorkTable*(nonogram: Nonogram, solver: NonogramSolver): WorkTable =
+#" getters for hide values
+proc endTime*(wt: WorkTable): float {.inline.} =
+  wt.endTime
+
+## Function to create a new WorkTable
+proc newWorkTable*(nonogram: Nonogram): WorkTable =
   result.nonogram = nonogram
   result.startTime = cpuTime()
+  result.endTime = 0.0
   result.coloringLog = newColoringLog(nonogram)
-  result.solver = solver
+  result.totalUnknown = nonogram.numRows * nonogram.numCols
+  result.rowUnknown = newSeq[int](nonogram.numRows)
+  result.colUnknown = newSeq[int](nonogram.numCols)
+  
+  for row in 0..<nonogram.numRows:
+    result.rowUnknown[row] = nonogram.numCols
+  for col in 0..<nonogram.numRows:
+    result.colUnknown[col] = nonogram.numRows
+
   return result
 
-# stopTime measures the elapsed time
+## stopTime measures the elapsed time
 proc stopTimer*(workTable: var WorkTable) =
   workTable.endTime = cpuTime() - workTable.startTime
+
+## updateCellState will do three jobs
+## 1. Change the cell state from unkown to black or white
+## 2. Record the coloringLog
+## 3. Update the counts of unkown cells
+proc updateCellState*(workTable: var WorkTable, row: int, col: int, state: CellState): bool = 
+  if workTable.nonogram.setCellState(row, col, state):
+    workTable.coloringLog.addLast((rowInd: row, colInd: col, color: state))
+    workTable.totalUnknown -= 1
+    workTable.rowUnknown[row] -= 1
+    workTable.colUnknown[col] -= 1
+    return true
+  else:
+    return false
