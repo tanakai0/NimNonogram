35f7dc0cb48b828b13259017a865d258f4a6b511
tanakai0 <tanakatanakakai@gmail.com>
2024-07-01 12:51:41 +0900

    rename and update preprocessingLine

--- src/NimNonogrampkg/solverspkg/heuristicLogicSolver.nim
+++ src/NimNonogrampkg/solverspkg/heuristicLogicSolver.nim
@@ -5,67 +5,76 @@ References
 Proc of the 2007 IEEE Symposium on Computational Intelligence and Games, 2007.
 ]#
 
+import math
 import solvers
 import ../[utils, nonogram, workTable]
 
 type
-  PreprocessingLogicSolver* = ref object of NonogramSolver
+  HeuristicPreprocessingSolver* = ref object of NonogramSolver
 
-proc newPreprocessingLogicSolver*(workTable: WorkTable): PreprocessingLogicSolver = 
-  result = PreprocessingLogicSolver(workTable: workTable)
+proc newHeuristicPreprocessingSolver*(workTable: var WorkTable): HeuristicPreprocessingSolver = 
+  result = HeuristicPreprocessingSolver(workTable: workTable)
   return result
 
-proc preprocessingLogicRow(workTable: WorkTable, row: int) = 
-  echo workTable.endTime
+proc preprocessingLine(workTable: var WorkTable, lineIndex: int, asRow: bool) = 
+  var
+    hint: seq[int]
+    lineLength: int
+  if asRow:
+    hint = workTable.nonogram.rowHints[lineIndex]
+    lineLength = workTable.nonogram.numCols
+  else:
+    hint = workTable.nonogram.colHints[lineIndex]
+    lineLength = workTable.nonogram.numRows
+  var
+    states: seq[CellState] = newSeq[CellState](lineLength)
+  
+  # When the all cells are white
+  if hint == []:
+    for i in 0 ..< lineLength:
+      states[i] = white
+    discard workTable.updateLineStates(lineIndex, states, asRow)
+    return
 
-method solve*(solver: PreprocessingLogicSolver): bool = 
+  # When the description just fit in the line.
+  if sum(hint) + len(hint) - 1 == lineLength:
+    var index = 0
+    for i, h in hint:
+      for j in 0..<h:
+        states[index] = black
+        index += 1
+      if i != len(hint) - 1:
+        states[index] = white
+        index += 1
+    discard workTable.updateLineStates(lineIndex, states, asRow)
+    return
+
+  # Other cases
+
+  #     l_l, l_d = len(line), len(d)
+#     z = l_l - sum(d) - l_d + 1
+    
+#     # z < 0 means that it is inconsistent
+#     if z < 0:
+#         return(-1)
+    
+#     new_line = line
+#     ls, le, rs = 0, 0, 0
+#     for k in range(l_d):
+#         ls = 0 if k == 0 else le + 2
+#         le = ls + d[k] - 1
+#         rs = ls + z
+#         for x in range(rs, le + 1):
+#             new_line[x] = black
+            
+#     return new_line
+
+method solve*(solver: HeuristicPreprocessingSolver): bool = 
   for row in 0 ..< solver.workTable.nonogram.numRows:
-    preprocessingLogicRow(solver.workTable, row)
+    preprocessingLine(solver.workTable, row, true)
   return solver.workTable.nonogram.isSolved()
 
 
-
-# def preprocessing_logic(puzzle):
-#     """
-#     This sets obvious values for a grid, not considering colored cells.
-
-#     Parameters
-#     ----------
-#     puzzle : nonogram.Puzzle
-#         Target puzzle.
-    
-#     Returns
-#     -------
-#     None
-#     """ 
-#     for i in range(puzzle.m):
-#         row, d = puzzle.copy_row(i), puzzle.get_dr(i)
-#         new_row = preprocessing_logic_line(row, d, puzzle.black, puzzle.white)
-#         if new_row == -1:
-#             raise ValueError("dr[{}] isn't appropriate by function preprocessing_logic".format(i))
-#         puzzle.set_row(i, new_row)
-#     for j in range(puzzle.n):
-#         col, d = puzzle.copy_col(j), puzzle.get_dc(j)
-#         new_col = preprocessing_logic_line(col, d, puzzle.black, puzzle.white)
-#         if new_col == -1:
-#             raise ValueError("dc[{}] isn't appropriate by function preprocessing_logic".format(j))
-#         puzzle.set_col(j, new_col)
-
-        
-# def preprocessing_logic_line(line, d, black, white):
-#     """
-#     This sets obvious values for a one line (row or colmun), not considering colored cells.
-
-#     Parameters
-#     ----------
-#     line : list of int
-#         1-dimensional list which express the row or column.
-#     d    : list of list of int
-#         Description of the line.
-#     black : int
-#         Symbol of a cell that is colored black.
-#     white : int
-#         Symbol of a cell that is colored white.
     
 #     Returns
 #     -------
@@ -98,20 +107,7 @@ method solve*(solver: PreprocessingLogicSolver): bool =
 #     Thus,
 #     xxxxx1111111xxxx1xxx
 #     """
-    
-#     # when the all cells are white 
-#     if d == []:
-#         return [white for _ in range(len(line))]
-    
-#     # when the description only fit in line
-#     if sum(d) + len(d) - 1 == len(line):
-#         ret = []
-#         for (i, d_i) in enumerate(d):
-#             for j in range(d_i):
-#                 ret.append(black)
-#             if i != len(d) - 1:
-#                 ret.append(white)
-#         return ret
+
         
 #     l_l, l_d = len(line), len(d)
 #     z = l_l - sum(d) - l_d + 1
