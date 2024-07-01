0da2285c0b78576fae884e4b2ac8d8e6e504f27b
tanakai0 <tanakatanakakai@gmail.com>
2024-07-01 12:52:05 +0900

    init

---
+++ tests/testHeuristicLogicSolver.nim
@@ -0,0 +1,6 @@
+import std/unittest
+import NimNonogrampkg/[nonogram, constants]
+import NimNonogrampkg/solverspkg/heuristicLogicSolver
+
+suite "heuristicLogicSolver Tests":
+  test "correct preprocessingLineLogic":
