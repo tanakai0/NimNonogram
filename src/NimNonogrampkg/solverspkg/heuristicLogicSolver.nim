42329eabe0f1412a1a3e72ca8f432bd1109bdd70
tanakai0 <tanakatanakakai@gmail.com>
2024-07-03 22:19:13 +0900

    try to make an additional function

--- src/NimNonogrampkg/solverspkg/heuristicLogicSolver.nim
+++ src/NimNonogrampkg/solverspkg/heuristicLogicSolver.nim
@@ -106,3 +106,161 @@ method solve*(solver: HeuristicPreprocessingSolver): bool =
   for col in 0 ..< solver.workTable.nonogram.numCols:
     preprocessingLine(solver.workTable, col, false)
   return solver.workTable.nonogram.isSolved()
+
+
+
+# https://nim-lang.org/docs/manual.html#iterators-and-the-for-statement-firstminusclass-iterators
+
+# def gen_enumerate_all_coloring(line, d, black, white, blank):
+#     """
+#     generator that returns all coloring pattern of the line considering restriction of filled cells
+
+#     Parameters
+#     ----------
+#     line : list of int
+#         1-dimensional list which express row or column
+#     d    : list of list of int
+#         description of the line
+#     black : int
+#         symbol of a cell that is colored black
+#     white : int
+#         symbol of a cell that is colored white
+#     blank : int
+#         symbol of a cell that is not decided yet
+    
+#     Yield
+#     -----
+#     filled_line : list of int
+#         filled pattern
+#     """
+#     # set initial value for other function
+#     ret = [blank for _ in range(len(line))]
+#     yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, 0, 0, ret)
+    
+# def _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos, d_ind, ret):
+#     """
+#      generator that returns all coloring pattern of the line using recursive and restriction of filled cells
+
+#     Parameters
+#     ----------
+#     line : list of int
+#         1-dimensional list which express row or column
+#     d    : list of list of int
+#         description of the line
+#     black : int
+#         symbol of a cell that is colored black
+#     white : int
+#         symbol of a cell that is colored white
+#     blank : int
+#         symbol of a cell that is not decided yet
+#     pos : int
+#         previous position of line (or ret) (used only for recursive) 
+#     d_ind : int
+#         previous position of description d (used only for recursive)
+#     ret : list of int
+#         temporary try of left most justification (used only for recursive)
+    
+#     Yield
+#     -----
+#     filled_line : list of int
+#         filled pattern
+        
+#     Notes
+#     -----
+#     abstract of steps
+#     step1 : check if the solution is right and yield ret when it is a consistent pattern
+#     step2 : decide the element of the description and try the all positions of the elements 
+#     """
+#     # print("pos:{} d_ind:{}\n ret:{}\n--------------------------".format(pos, d_ind, ret))
+    
+#     # set initial values for this function
+#     if ret == None:
+#         ret = [[blank for _ in range(len(line))]]
+        
+#     # when all elements of the description was adopted
+#     if d_ind == len(d):
+#         if blank not in ret and nonogram.Puzzle.line2description(ret, black, white, blank, focus = black) == d:
+#             yield deepcopy(ret)
+            
+#     # when some elements of the description wasn't adopted
+#     else:
+#         copy_ret = deepcopy(ret)
+#         # name value which occurs frequently black_range
+#         black_range = pos + d[d_ind]
+        
+#         # if d_ind is last index
+#         if d_ind == len(d) - 1:
+#             # obvious inconsistent check without coloring any 
+#             if black_range <= len(line):
+#                 if line[pos] == black:
+#                     # obvious inconsistent check without coloring any cell
+#                     if (white not in line[pos + 1: black_range]) and (black not in line[black_range:]):
+#                         for i in range(pos, black_range):
+#                             ret[i] = black
+#                         for i in range(black_range, len(line)):
+#                             ret[i] = white
+#                         yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos = len(line), d_ind = d_ind + 1, ret = ret)
+#                 elif line[pos] == white:
+#                     ret[pos] = white
+#                     yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos +1 , d_ind = d_ind, ret = ret)
+#                 else:
+#                     # color black for now
+#                     if (white not in line[pos + 1: black_range]) and (black not in line[black_range:]):
+#                         for i in range(pos, black_range):
+#                             ret[i] = black
+#                         for i in range(black_range, len(line)):
+#                             ret[i] = white
+#                         yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos = len(line), d_ind = d_ind + 1, ret = ret)
+#                     # if black is inconsistent, then color white now with modifying coloring
+#                     for i in range(len(ret)):
+#                         ret[i] = copy_ret[i]
+#                     ret[pos] = white
+#                     yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos + 1, d_ind = d_ind, ret = ret)
+#         # if d_ind isn't last index          
+#         else:
+#             # obvious inconsistent check without coloring any cell
+#             if pos + sum(d[d_ind:]) + len(d) - d_ind - 1 <= len(line):
+#                 if line[pos] == black:
+#                     # obvious inconsistent check without coloring any cell
+#                     if (white not in line[pos + 1: black_range]) and (black != line[black_range]):
+#                         for i in range(pos, black_range):
+#                             ret[i] = black
+#                         ret[black_range] = white
+#                         yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos = black_range + 1, d_ind = d_ind + 1, ret = ret)
+#                 elif line[pos] == white:
+#                     ret[pos] = white
+#                     yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos = pos + 1, d_ind = d_ind, ret = ret)
+#                 else:
+#                     # color black for now
+#                     if (white not in line[pos + 1: black_range]) and (black != line[black_range]):
+#                         for i in range(pos, black_range):
+#                             ret[i] = black
+#                         ret[black_range] = white
+#                         yield from _rec_gen_enumerate_all_coloring(line, d, black, white, blank, pos = black_range + 1, d_ind = d_ind + 1, ret = ret)
+
+
+## left_most_justification returns the left-most justigication for one line
+## Example
+## -------
+## 0 : white, 1 : black, 2 : blank, 
+## length of the line is 21 (index 0-20), d = [2,2,2,3,1] = [d0,d1,d2,d3,d4]
+## 011020222022122221201  original
+## 011000110001100111001  left most justification
+## 
+proc left_most_justification*(line: seq[CellState], hint: seq[int]): seq[CellState] = 
+  var
+    lineLength: int = len(line)
+  if hint == []:
+    for _ in 0 ..< lineLength:
+      result.add(white)
+      return result
+#     g = gen_enumerate_all_coloring(line, d, black, white, blank)
+#     # first element of g is the left-most justification
+#     left_most = None
+#     try:
+#         left_most = g.__next__()
+#     except StopIteration:  # if there isn't left-most justification
+#         left_most = -1
+#     g.close()
+#     return left_most
+
