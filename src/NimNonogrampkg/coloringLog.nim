b236526ee74669ba135bbb5380f2a83bb565a992
tanakai0 <tanakatanakakai@gmail.com>
2024-08-02 08:37:10 +0900

    add a proc getLastN

--- src/NimNonogrampkg/coloringLog.nim
+++ src/NimNonogrampkg/coloringLog.nim
@@ -19,3 +19,26 @@ proc push*(coloringLog: var ColoringLog, row: int, col: int, color: CellState) =
   if color == unknown:
     raise newException(ValueError, "ColoringLog cannot contain unknown.")
   coloringLog.addLast((row: row, col: col, color: color))
+
+# Function to get the last n elements from the ColoringLog
+proc getLastN*(coloringLog: ColoringLog, n: int): seq[CellIndexColor] =
+  if coloringLog.len < n:
+    raise newException(ValueError, "Not enough elements in ColoringLog to retrieve last n elements.")
+  result = @[]
+  for i in 0..<n:
+    result.add(coloringLog[coloringLog.len - i - 1])
+
+
+when isMainModule:
+  let nono = newNonogram(2, 2, @[@[1], @[1]], @[@[1], @[1]])
+  var log = newColoringLog(nono)
+  
+  log.push(0, 0, black)
+  log.push(1, 1, white)
+  log.push(2, 2, black)
+  
+  try:
+    let lastTwo = log.getLastN(1)
+    echo lastTwo
+  except ValueError as e:
+    echo e.msg
