6d6e46a91911abc40c2bd286acb1dd15cbc8e391
tanakai0 <tanakatanakakai@gmail.com>
2024-08-01 23:14:16 +0900

    update updateRowStates, updateColStates, and updateLineStates to return the updated number of cells

--- src/NimNonogrampkg/workTable.nim
+++ src/NimNonogrampkg/workTable.nim
@@ -70,6 +70,7 @@ proc stopTimer*(workTable: var WorkTable) =
 ##
 ## Returns:
 ## - bool: True if the cell state was successfully updated and logged, false otherwise.
+## Apparent contradiction raises an exception. So, this value regards to the number of newely filled cells. 
 proc updateCellState*(workTable: var WorkTable, row: int, col: int, state: CellState): bool = 
   if workTable.nonogram.setCellState(row, col, state):
     workTable.coloringLog.push(row, col, state)
@@ -80,7 +81,7 @@ proc updateCellState*(workTable: var WorkTable, row: int, col: int, state: CellS
   else:
     return false
 
-## updateRowStates updates the states of an entire row in the Nonogram grid.
+## updateRowStates updates the states of a row in the Nonogram grid.
 ## It uses updateCellState to set each cell in the row and update the WorkTable accordingly.
 ##
 ## Parameters:
@@ -89,20 +90,21 @@ proc updateCellState*(workTable: var WorkTable, row: int, col: int, state: CellS
 ## - states: A sequence of CellState values to set for the specified row.
 ##
 ## Returns:
-## - bool: True if at least one cell state was successfully updated, false otherwise.
-proc updateRowStates*(workTable: var WorkTable, row: int, states: seq[CellState]): bool = 
+## - int: The number of newely filled cells. 
+proc updateRowStates*(workTable: var WorkTable, row: int, states: seq[CellState]): int = 
   var
-    updateAtLeastOne: bool = false
+    updateNum: int = 0
     updateResult: bool
+    # updateAtLeastOne: bool = false
   for col in 0 ..< workTable.nonogram.numCols:
     updateResult = workTable.updateCellState(row, col, states[col])
-    updateAtLeastOne = (updateAtLeastOne or updateResult)
+    updateNum += ord(updateResult)
     # updateAtLeastOne = (updateAtLeastOne or workTable.updateCellState(row, col, states[col]))  will not work as the way you want.
     # Nim uses the short-circuit evaluation?
     # If the updateAtLeastOne is true, workTable.updateCellState(row, col, states[col]) is not evaluated. So the proc will not be activated.
-  return updateAtLeastOne
+  return updateNum
 
-## updateColStates updates the states of an entire col in the Nonogram grid.
+## updateColStates updates the states of a column in the Nonogram grid.
 ## It uses updateCellState to set each cell in the col and update the WorkTable accordingly.
 ##
 ## Parameters:
@@ -111,18 +113,23 @@ proc updateRowStates*(workTable: var WorkTable, row: int, states: seq[CellState]
 ## - states: A sequence of CellState values to set for the specified col.
 ##
 ## Returns:
-## - bool: True if at least one cell state was successfully updated, false otherwise.
-proc updateColStates*(workTable: var WorkTable, col: int, states: seq[CellState]): bool = 
+## - int: The number of newely filled cells. 
+proc updateColStates*(workTable: var WorkTable, col: int, states: seq[CellState]): int = 
   var 
-    updateAtLeastOne: bool = false
+    updateNum: int = 0
     updateResult: bool
+    # updateAtLeastOne: bool = false
   for row in 0 ..< workTable.nonogram.numRows:
     updateResult = workTable.updateCellState(row, col, states[row])
-    updateAtLeastOne = (updateAtLeastOne or updateResult)
-  return updateAtLeastOne
+    updateNum += ord(updateResult)
+    # updateAtLeastOne = (updateAtLeastOne or updateResult)
+  return updateNum
 
-
-proc updateLineStates*(workTable: var WorkTable, lineIndex: int, states: seq[CellState], asRow: bool): bool = 
+## updateLineStates updates the states of a line in the Nonogram grid.
+## It uses updateCellState to set each cell in the col and update the WorkTable accordingly.
+## Returns:
+## - int: The number of newely filled cells. 
+proc updateLineStates*(workTable: var WorkTable, lineIndex: int, states: seq[CellState], asRow: bool): int = 
   if asRow:
     return updateRowStates(workTable, lineIndex, states)
   else:
