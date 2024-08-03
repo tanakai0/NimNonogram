86fec0bd3c5250c0d28aaa219d84906617d47eef
tanakai0 <tanakatanakakai@gmail.com>
2024-08-03 14:23:29 +0900

    add the color symbols

--- src/NimNonogrampkg/constants.nim
+++ src/NimNonogrampkg/constants.nim
@@ -1,1 +1,4 @@
 const ExamplePuzzlePath*: string = "db/example.non"
+const WhiteSymbol*: int = 0  # The cell is white (empty).
+const BlackSymbol*: int = 1  # The cell is black (filled).
+const UnknownSymbol*: int = 2  # The cell's state is not yet determined.
